83619498ae28d6293c759a72560c7fddcd238951
VermanEZ <kudinov.nikita@gmail.com>
2021-10-02 08:42:53 +0300

    Database initialized

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost:27017/base')
+
+const Schema = mongoose.Schema
+const ProductsSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        minlength: 2
+    },
+    price: Number,
+    left: {
+        type: Number,
+        default: 0
+    }
+})
+
+const Product = mongoose.model('products', ProductsSchema)
 
 
 app.get('/', (req, res) => {
