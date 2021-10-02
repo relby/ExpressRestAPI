57811764bc14bfbbd92aefcb6c84057bffcbb53b
VermanEZ <kudinov.nikita@gmail.com>
2021-10-02 09:40:58 +0300

    Added GET method to get all products

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// Includes
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+// Constants
 const PORT = 3000
 
+// DB
 mongoose.connect('mongodb://localhost:27017/base')
 
 const Schema = mongoose.Schema
@@ -11,6 +14,7 @@ const ProductsSchema = new Schema({
     name: {
         type: String,
         trim: true,
+        unique: true,
         minlength: 2
     },
     price: Number,
@@ -22,10 +26,23 @@ const ProductsSchema = new Schema({
 
 const Product = mongoose.model('products', ProductsSchema)
 
+// Middleware
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("test")
+// Routes
+app.get('/api/products', (req, res) => {
+    Product.find({}, (err, products) => {
+        if (err) return console.error(err);
+        products.map((value, index) => {
+            products[index] = {
+                name: value.name,
+                price: value.price,
+                left: value.left,
+            }
+        })
+        res.status(200).json(products)
+    })
 })
 
-
+// Server start
 app.listen(PORT, () => console.log(`Server has been started on http://localhost:${PORT}`))
